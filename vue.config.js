4fa5faea0a0e6ce38adbfbc6f581deeeb6f801d1
Illia Osmanov <illia.osmanov@stfalcon.com>
2019-10-24 03:21:26 +0300

    Purge cache

--- vue.config.js
+++ vue.config.js
@@ -93,7 +93,7 @@ module.exports = {
 
         renderer: new Renderer({
           maxConcurrentRoutes: 4,
-          headless: true,
+          headless: false,
           renderAfterDocumentEvent: "render-event",
           args: ["--no-sandbox"],
         }),
