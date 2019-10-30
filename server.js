e0b5fec775c0e8c3b4f6f689e80f9dcd22948231
Illia Osmanov <illia.osmanov@stfalcon.com>
2019-10-30 02:45:50 +0200

    remove http server

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const serveStatic = require("serve-static");
 const port = process.env.PORT || 5000;
 const app = express();
 
-// app.use("/", expressStaticGzip(path.join(`${__dirname}/dist/`), {
-//   enableBrotli: true,
-//   orderPreference: ["br"],
-// }));
+app.use("/", expressStaticGzip(path.join(`${__dirname}/dist/`), {
+  enableBrotli: true,
+  orderPreference: ["br"],
+}));
 
-app.use(express.static(path.join(__dirname, "/dist/")));
-app.get(/.*/, (_, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
 });
 
